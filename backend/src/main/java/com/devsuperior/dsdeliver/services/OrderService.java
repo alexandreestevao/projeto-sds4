4fe5386f5a2c2019b5a5306b239a0799b60a94c0
Alexandre Estevão <alexandresestevao@gmail.com>
2022-02-05 21:29:24 -0300

    GET /orders

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired // Container de injeção de independência que executa de forma transparente
+	private OrderRepository orderRepository;
+	
+	@Transactional(readOnly = true) // Somente leitura e abre e fecha a transação transparente
+	public List<OrderDTO> findAll() {
+		List<Order> list = orderRepository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
