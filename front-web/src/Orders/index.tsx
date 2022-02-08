ff35c6479c842272e9890c2953121dfde21685be
Alexandre Estevão <alexandresestevao@gmail.com>
2022-02-07 22:12:17 -0300

    Add Mapbox

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { fetchProducts } from '../api';
+import './styles.css';
 import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
-
-import './styles.css';
-import { Product } from './types';
+import { fetchProducts } from '../api';
+import { OrderLocationData, Product } from './types';
+import OrderLocation from './OrderLocation';
 
 function Orders() {
-
     const [products, setProducts] = useState<Product[]>([]);
-
+    const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    
     useEffect(() => {
         fetchProducts()
             .then(response => setProducts(response.data))
@@ -20,6 +20,7 @@ function Orders() {
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
         </div>
     )
 }
